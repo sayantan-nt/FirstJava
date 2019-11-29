5874eaf64bf85904d62fbe7dc347e5c7c5cd0fd4
sayantan-nt <58296232+sayantan-nt@users.noreply.github.com>
2019-11-29 12:34:09 +0530

    Update FirstCode.java

--- FirstCode.java
+++ FirstCode.java
@@ -1,4 +1,14 @@
+public class FirstCd {
+public static void main (String a[]) {
+	char c = 'a';
+System.out.print(c);
+int i = 0B101;
+System.out.println("\n" + i );
 
-public class FirstCode {
+int b=5;
+int c1=b++;
+int d=++b;
+System.out.println("value of b is now :"+b+ ",value of c is :"+c1+" and value of d is :"+d);
 
+}
 }
